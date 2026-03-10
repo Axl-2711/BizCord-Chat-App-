8de7114117b38af371c638388a573e428275e820
Srajal Patel <srajal.omi@gmail.com>
2026-03-10 14:43:57 +0530

    messages.js added

---
+++ server/public/utils/messages.js
@@ -0,0 +1,14 @@
+export const formatMessage = (username, text) => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const amOrPm = hours >= 12 ? 'pm' : 'am';
+    const formattedTime = `${hours % 12 || 12}:${minutes.toString().padStart(2, '0')} ${amOrPm}`;
+
+    return {
+        username,
+        text,
+        time: formattedTime,
+    };
+};
+
